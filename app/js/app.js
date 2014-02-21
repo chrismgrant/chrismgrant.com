79324d2f2da663cad730797f5f56b50ae9a61393
Christopher Grant <cgrant@andrew.cmu.edu>
2014-02-20 16:44:37 -0800

    added new partials, started work on works page, added links to bio

--- app/js/app.js
+++ app/js/app.js
@@ -15,6 +15,12 @@ config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/thoughts', {templateUrl: 'partials/thoughts.html'});
 	$routeProvider.when('/resume', {templateUrl: 'partials/resume.html'});
 	$routeProvider.when('/works', {templateUrl: 'partials/works.html'});
+	$routeProvider.when('/lyft', {templateUrl: 'partials/lyft.html'});
+	$routeProvider.when('/circle', {templateUrl: 'partials/circle.html'});
+	$routeProvider.when('/jawbone', {templateUrl: 'partials/jawbone.html'});
+	$routeProvider.when('/maya', {templateUrl: 'partials/maya.html'});
+	$routeProvider.when('/other', {templateUrl: 'partials/other.html'});	
+	$routeProvider.when('/photography', {templateUrl: 'partials/photography.html'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
 
@@ -23,7 +29,10 @@ function IntroCtrl($scope){
 		{value: 1, name: 'User Experience Designer'},
 		{value: 2, name: 'Product Designer'},
 		{value: 3, name: 'Front-end Developer'},
-		{value: 4, name: 'Product Manager'}
+		{value: 4, name: 'Photographer'},
+		{value: 5, name: 'Cyclist'},
+		{value: 6, name: 'Denim Enthusiast'},
+		{value: 7, name: 'Extreme Pillow Fighter'}
 	];
 	$scope.position = $scope.positions[0];
 }
